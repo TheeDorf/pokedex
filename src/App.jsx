92e655e7bc92dd925b68fac59c3c9e49945c9835
AJ Dorfman <ajdorfman91@gmail.com>
2023-04-14 11:48:15 -0700

    start pokedex code

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filterTypes, setFilterTypes] = useState([]);
+  const [filterWeaknesses, setFilterWeaknesses] = useState([]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
