507ed712b7f623c1814e4b78cde0bc829e10894f
AJ Dorfman <ajdorfman91@gmail.com>
2023-04-17 10:23:08 -0700

    fix code to work

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,115 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
   const [pokemonList, setPokemonList] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
-  const [filterTypes, setFilterTypes] = useState([]);
-  const [filterWeaknesses, setFilterWeaknesses] = useState([]);
-
-
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filterTypes, setFilterTypes] = useState("");
+  const [filterWeaknesses, setFilterWeaknesses] = useState("");
+  let weakArr = [
+    "Grass",
+    "Fire",
+    "Water",
+    "Bug",
+    "Normal",
+    "Poison",
+    "Electric",
+    "Ground",
+    "Fairy",
+    "Fighting",
+    "Psychic",
+    "Rock",
+    "Ghost",
+    "Ice",
+    "Dragon",
+    "Dark",
+    "Steel",
+    "Flying",
+  ];
+  let typesArr = [
+    "Grass",
+    "Fire",
+    "Water",
+    "Bug",
+    "Normal",
+    "Poison",
+    "Electric",
+    "Ground",
+    "Fairy",
+    "Fighting",
+    "Psychic",
+    "Rock",
+    "Ghost",
+    "Ice",
+    "Dragon",
+    "Dark",
+    "Steel",
+    "Flying",
+  ];
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch('https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json');
+      const response = await fetch(
+        "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json"
+      );
       const data = await response.json();
-      console.log(data)
+      console.log(data);
       setPokemonList(data.pokemon);
     };
     fetchData();
   }, []);
 
-  const handleSearch = event => {
+  const handleSearch = (event) => {
     setSearchQuery(event.target.value.toLowerCase());
   };
 
-  const handleTypeFilter = event => {
-    const type = event.target.value;
-    if (event.target.checked) {
-      setFilterTypes([...filterTypes, type]);
-    } else {
-      setFilterTypes(filterTypes.filter(t => t !== type));
-    }
+  const handleTypeFilter = (event) => {
+    let type = event.target.value;
+
+    setFilterTypes(type);
+    console.log(filterTypes);
   };
 
-  const handleWeaknessFilter = event => {
-    const weakness = event.target.value;
-    if (event.target.checked) {
-      setFilterWeaknesses([...filterWeaknesses, weakness]);
-    } else {
-      setFilterWeaknesses(filterWeaknesses.filter(w => w !== weakness));
-    }
+  const handleWeaknessFilter = (event) => {
+    let weakness = event.target.value;
+    setFilterWeaknesses(weakness);
+    console.log(filterWeaknesses);
   };
 
-  const filteredPokemon = pokemonList.filter(pokemon =>
-    (pokemon.name.toLowerCase().includes(searchQuery) ||
-     pokemon.num.includes(searchQuery)) &&
-    ((filterTypes.length === 0 || filterTypes.some(type => pokemon.type.includes(type)))) &&
-    ((filterWeaknesses.length === 0 || filterWeaknesses.some(weakness => pokemon.weaknesses.includes(weakness))))
-  );
+  let test = (searchQuery, filterTypes, filterWeaknesses) => {
+    console.log(searchQuery, filterTypes, filterWeaknesses);
+    if (searchQuery && filterTypes && filterWeaknesses) {
+      let value = pokemonList.filter((pokemon) => {
+        return pokemon.name.includes(searchQuery) || pokemon.num.includes(searchQuery) && pokemon.type.includes(filterTypes) && pokemon.weaknesses.includes(filterWeaknesses)
+      });
+      return value;
+    }
+    else if (filterTypes && filterWeaknesses) {
+      let value = pokemonList.filter((pokemon) => {
+        return pokemon.type.includes(filterTypes) && pokemon.weaknesses.includes(filterWeaknesses)
+      });
+      return value;
+    }
+    else if (filterTypes || filterWeaknesses) {
+      let value = pokemonList.filter((pokemon) => {
+        return pokemon.type.includes(filterTypes) || pokemon.weaknesses.includes(filterWeaknesses)
+      });
+      return value;
+    } else if (searchQuery) {
+      let value = pokemonList.filter((pokemon) => {
+        return pokemon.name.toLowerCase().startsWith(searchQuery) || pokemon.num.startsWith(searchQuery)
+      });
+      return value;
+    }else {
+      return pokemonList;
+    }
+  };
+  let filteredPokemon = test(searchQuery, filterTypes, filterWeaknesses);
+  console.log(filteredPokemon);
+
+  // (pokemon.name.toLowerCase().includes(searchQuery) ||
+  //  pokemon.num.includes(searchQuery)) &&
+  // ((pokemon.some(creature => creature.type.includes(filterTypes)))) &&
+  // ((pokemon.some(creature => creature.weaknesses.includes(filterWeaknesses))))
 
   return (
     <div className="App">
@@ -53,33 +117,50 @@ function App() {
       <div className="filters">
         <label>
           Search by name or number:
-          <input type="text" placeholder="enter a number" value={searchQuery} onChange={handleSearch} />
+          <input
+            type="text"
+            placeholder="Number or Letter"
+            value={searchQuery}
+            onChange={handleSearch}
+          />
         </label>
         <label>
           Filter by type:
-          <select multiple value={filterTypes} onChange={handleTypeFilter}>
-            {['Grass', 'Fire', 'Water', 'Bug', 'Normal', 'Poison', 'Electric', 'Ground', 'Fairy', 'Fighting', 'Psychic', 'Rock', 'Ghost', 'Ice', 'Dragon', 'Dark', 'Steel', 'Flying'].map(type => (
-              <option key={type} value={type}>{type}</option>
+          <select value={filterTypes} onChange={handleTypeFilter}>
+            <option value="" key="All">
+              All
+            </option>
+            {typesArr.map((type) => (
+              <option key={type} value={type}>
+                {type}
+              </option>
             ))}
           </select>
         </label>
         <label>
           Filter by weakness:
-          <select multiple value={filterWeaknesses} onChange={handleWeaknessFilter}>
-            {['Grass', 'Fire', 'Water', 'Bug', 'Normal', 'Poison', 'Electric', 'Ground', 'Fairy', 'Fighting', 'Psychic', 'Rock', 'Ghost', 'Ice', 'Dragon', 'Dark', 'Steel', 'Flying'].map(weakness => (
-              <option key={weakness} value={weakness}>{weakness}</option>
+          <select value={filterWeaknesses} onChange={handleWeaknessFilter}>
+            <option value="" key="All">
+              All
+            </option>
+            {weakArr.map((weakness) => (
+              <option key={weakness} value={weakness}>
+                {weakness}
+              </option>
             ))}
           </select>
         </label>
       </div>
       <ul className="pokemon-list">
-        {filteredPokemon.map(pokemon => (
+        {filteredPokemon.map((pokemon) => (
           <li key={pokemon.num}>
-            <h2>{pokemon.name} ({pokemon.num})</h2>
+            <h2>
+              {pokemon.name} ({pokemon.num})
+            </h2>
             <img src={pokemon.img} alt={pokemon.num} />
-            <p>Type: {pokemon.type.join(', ')}</p>
-            <p>Weaknesses: {pokemon.weaknesses.join(', ')}</p>
-            </li>
+            <p>Type: {pokemon.type.join(", ")}</p>
+            <p>Weaknesses: {pokemon.weaknesses.join(", ")}</p>
+          </li>
         ))}
       </ul>
     </div>
@@ -87,17 +168,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
